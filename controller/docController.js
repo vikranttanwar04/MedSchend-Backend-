eb939f61c682f562d96cdeeac3910716bc787c6e
Vikrant Tanwar <151028037+vikranttanwar04@users.noreply.github.com>
2025-11-20 22:52:34 +0530

    Change cookie SameSite attribute to 'none'

--- controller/docController.js
+++ controller/docController.js
@@ -63,7 +63,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
         })
 
         res.status(200).json({ message: "Login Successful." });
