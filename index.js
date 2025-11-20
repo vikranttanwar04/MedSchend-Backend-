d4122ad0dfe258459ee0f0900f9ad0f46aca94d3
Vikrant Tanwar <151028037+vikranttanwar04@users.noreply.github.com>
2025-11-20 22:49:17 +0530

    Change CORS origin to production URL

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://med-schend-frontend.vercel.app/",
     credentials: true
 }));
 
