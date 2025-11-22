e48605077488b384c360d2b8a40cdacb5b803921
Vikrant Tanwar <vikranttanwar12345@gmail.com>
2025-11-22 20:01:12 +0530

    Removing / from cors origin

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://med-schend-frontend.vercel.app/",
+    origin: "https://med-schend-frontend.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true
 }));
